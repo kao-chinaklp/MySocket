a52c4ae33d2f408dab9efa24cb1f22908fa91c4a
Mo Yongxin <1134200680@qq.com>
2023-07-14 19:42:30 +0800

    Revert "HHHHHH"
    This reverts commit e78fe021b43dc4a0e19d38e8f414fe9e2fe20582.

--- context.h
+++ context.h
@@ -1,5 +1,4 @@
-#define DefaultCfg                                                             \
-  "# socket（密钥文件留空表示自动生成，key-psw项留空表示使用默认密码123456）\n\
+#define DefaultCfg "# socket（密钥文件留空表示自动生成，key-psw项留空表示使用默认密码123456）\n\
                     cert=\nkey=\nkey-psw=\nsock-port=2333\nsock-que-size=10\n\n\
                     # mysql\nip=47.240.92.80\nusername=userinfo\npassword=mima123.\n\
                     dbname=userinfo\ndb-port=3306\ndb-que-size=5\n\n\
--- thread.cpp
+++ thread.cpp
@@ -3,130 +3,135 @@
 #include <chrono>
 #include <thread>
 
-void CTask::SetConnFd(int data) { connfd = data; }
-
-int CTask::GetConnFd() { return connfd; }
-
-void CTask::SetTaskName(const string _taskname) { TaskName = _taskname; }
-
-string CTask::GetTaskName() { return TaskName; }
-
-CThreadPool::CThreadPool(int threadNum) {
-  shutdown = false;
-  TaskNum = threadNum;
-  pthreadMutex = PTHREAD_MUTEX_INITIALIZER;
-  pthreadCond = PTHREAD_COND_INITIALIZER;
-  Create();
+void CTask::SetConnFd(int data){
+    connfd=data;
 }
 
-CThreadPool::~CThreadPool() {
-  StopAll();
-  pthread_mutex_destroy(&pthreadMutex);
-  pthread_cond_destroy(&pthreadCond);
+int CTask::GetConnFd(){
+    return connfd;
 }
 
-void *ThreadFunc(void *threadData) {
-  CThreadPool *Data = (CThreadPool *)threadData;
-  pthread_t tid = pthread_self();
-  while (true) {
-    pthread_mutex_lock(&Data->pthreadMutex);
-    // 没有任务，等待执行
-    while (Data->queTaskList.empty() && !Data->shutdown)
-      pthread_cond_wait(&Data->pthreadCond, &Data->pthreadMutex);
-    if (Data->queTaskList.empty() && Data->shutdown) {
-      pthread_mutex_unlock(&Data->pthreadMutex);
-      pthread_exit(NULL);
+void CTask::SetTaskName(const string _taskname){
+    TaskName=_taskname;
+}
+
+string CTask::GetTaskName(){
+    return TaskName;
+}
+
+CThreadPool::CThreadPool(int threadNum){
+    shutdown=false;
+    TaskNum=threadNum;
+    pthreadMutex=PTHREAD_MUTEX_INITIALIZER;
+    pthreadCond=PTHREAD_COND_INITIALIZER;
+    Create();
+}
+
+CThreadPool::~CThreadPool(){
+    StopAll();
+    pthread_mutex_destroy(&pthreadMutex);
+    pthread_cond_destroy(&pthreadCond);
+}
+
+void* ThreadFunc(void* threadData){
+    CThreadPool* Data=(CThreadPool*)threadData;
+    pthread_t tid=pthread_self();
+    while(true){
+        pthread_mutex_lock(&Data->pthreadMutex);
+        //没有任务，等待执行
+        while(Data->queTaskList.empty()&&!Data->shutdown)
+            pthread_cond_wait(&Data->pthreadCond, &Data->pthreadMutex);
+        if(Data->queTaskList.empty()&&Data->shutdown){
+            pthread_mutex_unlock(&Data->pthreadMutex);
+            pthread_exit(NULL);
+        }
+        CTask* task=Data->queTaskList.front();
+        Data->queTaskList.pop_front();
+        //取完任务后放锁
+        pthread_mutex_unlock(&Data->pthreadMutex);
+        Data->MoveToBusy(tid);
+        task->Run();
+        delete task;
+        Data->MoveToIdle(tid);
     }
-    CTask *task = Data->queTaskList.front();
-    Data->queTaskList.pop_front();
-    // 取完任务后放锁
-    pthread_mutex_unlock(&Data->pthreadMutex);
-    Data->MoveToBusy(tid);
-    task->Run();
-    delete task;
-    Data->MoveToIdle(tid);
-  }
-  return (void *)0;
+    return (void*)0;
 }
 
-int CThreadPool::MoveToIdle(pthread_t tid) {
-  deque<pthread_t>::iterator p = find(tid);
-  if (p == BusyQue.end())
-    return -1;
-  pthread_mutex_lock(&pthreadMutex);
-  BusyQue.erase(p);
-  pthread_mutex_unlock(&pthreadMutex);
-  return 1;
+int CThreadPool::MoveToIdle(pthread_t tid){
+    deque<pthread_t>::iterator p=find(tid);
+    if(p==BusyQue.end())return -1;
+    pthread_mutex_lock(&pthreadMutex);
+    BusyQue.erase(p);
+    pthread_mutex_unlock(&pthreadMutex);
+    return 1;
 }
 
-int CThreadPool::MoveToBusy(pthread_t tid) {
-  pthread_mutex_lock(&pthreadMutex);
-  BusyQue.push_back(tid);
-  pthread_mutex_unlock(&pthreadMutex);
-  return 1;
+int CThreadPool::MoveToBusy(pthread_t tid){
+    pthread_mutex_lock(&pthreadMutex);
+    BusyQue.push_back(tid);
+    pthread_mutex_unlock(&pthreadMutex);
+    return 1;
 }
 
-int CThreadPool::AddTask(CTask *task) {
-  pthread_mutex_lock(&pthreadMutex);
-  queTaskList.push_back(task);
-  pthread_mutex_unlock(&pthreadMutex);
-  // 发信号
-  pthread_cond_signal(&pthreadCond);
-  return 0;
+int CThreadPool::AddTask(CTask *task){
+    pthread_mutex_lock(&pthreadMutex);
+    queTaskList.push_back(task);
+    pthread_mutex_unlock(&pthreadMutex);
+    //发信号
+    pthread_cond_signal(&pthreadCond);
+    return 0;
 }
 
-int CThreadPool::Create() {
-  pthread_id = (pthread_t *)malloc(sizeof(pthread_t) * TaskNum);
-  for (int i = 0; i < TaskNum; i++)
-    pthread_create(&pthread_id[i], NULL, ThreadFunc, this);
-  return 0;
+int CThreadPool::Create(){
+    pthread_id=(pthread_t*)malloc(sizeof(pthread_t)*TaskNum);
+    for(int i=0;i<TaskNum;i++)
+        pthread_create(&pthread_id[i], NULL, ThreadFunc, this);
+    return 0;
 }
 
-void CThreadPool::Sleep(int ms) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+void CThreadPool::Sleep(int ms){
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-deque<pthread_t>::iterator CThreadPool::find(pthread_t tid) {
-  deque<pthread_t>::iterator p;
-  pthread_mutex_lock(&pthreadMutex);
-  for (p = BusyQue.begin(); p != BusyQue.end(); p++)
-    if (*p == tid)
-      break;
-  pthread_mutex_unlock(&pthreadMutex);
-  return p;
+deque<pthread_t>::iterator CThreadPool::find(pthread_t tid){
+    deque<pthread_t>::iterator p;
+    pthread_mutex_lock(&pthreadMutex);
+    for(p=BusyQue.begin();p!=BusyQue.end();p++)
+        if(*p==tid)break;
+    pthread_mutex_unlock(&pthreadMutex);
+    return p;
 }
 
-int CThreadPool::StopAll() {
-  if (shutdown)
-    return -1;
-  shutdown = true;
-  pthread_cond_broadcast(&pthreadCond);
-  deque<pthread_t> NoJoined;
-  // 阻塞所有线程
-  for (int i = 0; i < TaskNum; i++) {
-    if (find(pthread_id[i]) != BusyQue.end()) {
-      NoJoined.push_back(pthread_id[i]);
-      continue;
+int CThreadPool::StopAll(){
+    if(shutdown)return -1;
+    shutdown=true;
+    pthread_cond_broadcast(&pthreadCond);
+    deque<pthread_t>NoJoined;
+    //阻塞所有线程
+    for(int i=0;i<TaskNum;i++){
+        if(find(pthread_id[i])!=BusyQue.end()){
+            NoJoined.push_back(pthread_id[i]);
+            continue;
+        }
+        pthread_join(pthread_id[i], NULL);
     }
-    pthread_join(pthread_id[i], NULL);
-  }
-  while (!NoJoined.empty()) {
-    auto e = NoJoined.end();
-    for (auto p = NoJoined.begin(); p != e; p++)
-      if (find(*p) == BusyQue.end()) {
-        pthread_join(*p, NULL);
-        p = NoJoined.erase(p);
-        e = NoJoined.end();
-      }
-    this->Sleep(50);
-  }
-  free(pthread_id);
-  pthread_id = NULL;
-  return 0;
+    while(!NoJoined.empty()){
+        for(deque<pthread_t>::iterator p=NoJoined.begin();p!=NoJoined.end();p++)
+            if(find(*p)==BusyQue.end()){
+                pthread_join(*p, NULL);
+                p=NoJoined.erase(p);
+            }
+        this->Sleep(50);
+    }
+    free(pthread_id);
+    pthread_id=NULL;
+    return 0;
 }
 
-int CThreadPool::GetTaskSize() { return queTaskList.size(); }
+int CThreadPool::GetTaskSize(){
+    return queTaskList.size();
+}
 
-int CThreadPool::GetConnFd(int index) {
-  return queTaskList[index]->GetConnFd();
+int CThreadPool::GetConnFd(int index){
+    return queTaskList[index]->GetConnFd();
 }
